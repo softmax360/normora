fcf938231c2011747f0239ef1524969683c76b5f
rafuj <raihanrafuj9978@gmail.com>
2025-12-09 18:40:23 +0600

    fix error and suppresshydration

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,22 @@
-import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
 import "./globals.css";
 
 export const metadata = {
-  title: "Normora SaaS Landing Page",
-  description: "Normora Modern SaaS app built with Next.js",
+	title: "Normora SaaS Landing Page",
+	description: "Normora Modern SaaS app built with Next.js",
 };
 
 // className={`${geistSans.variable}`}
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body suppressHydrationWarning>
+				<Header />
+				<main>{children}</main>
+				<Footer />
+			</body>
+		</html>
+	);
 }
